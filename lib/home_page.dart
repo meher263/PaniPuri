0b515a47c802d7a4fccbffd8fa177dd302b4ff7c
Abhinav Damodara <adamodar2@gitam.in>
2023-08-04 07:17:11 +0530

    changed name

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,6 +9,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
+
   final List<Widget> _pages = [
     ProfilePage(),
     RewardsPage(),
@@ -17,28 +18,48 @@ class _MyHomePageState extends State<MyHomePage> {
     HistoryPage(),
   ];
 
+  PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Flyer App'),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        children: _pages,
       ),
-      body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
             _currentIndex = index;
+            _pageController.animateToPage(
+              index,
+              duration: Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+            );
           });
         },
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
             label: 'Profile',
+            backgroundColor: Colors.orange,
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.card_giftcard),
             label: 'Rewards',
+            backgroundColor: Colors.orange,
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -48,11 +69,12 @@ class _MyHomePageState extends State<MyHomePage> {
           BottomNavigationBarItem(
             icon: Icon(Icons.shopping_cart),
             label: 'Cart',
-            backgroundColor: Colors.black,
+            backgroundColor: Colors.orange,
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.history),
             label: 'History',
+            backgroundColor: Colors.orange,
           ),
         ],
       ),
@@ -64,8 +86,256 @@ class _MyHomePageState extends State<MyHomePage> {
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Profile',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.notifications,
+              color: Color(0xFFF27D28),
+            ),
+            onPressed: () {
+              // Handle notification button press
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 25,
+                ),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(242, 227, 219, 0.5),
+                    borderRadius: BorderRadius.circular(28),
+                  ),
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Stack(
+                        alignment: Alignment.bottomRight,
+                        children: [
+                          Container(
+                            width: 250,
+                            height: 250,
+                            decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(125),
+                              border: Border.all(
+                                width: 2,
+                                color: Colors.orange,
+                              ),
+                            ),
+                            child: Center(
+                              child: Icon(
+                                Icons.person, // Replace with your profile image
+                                size: 100,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            right: 0,
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.camera_alt,
+                                  color: Colors.orange,
+                                ),
+                                onPressed: () {
+                                  // Handle camera icon button press
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'Vedashakti',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 25,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'vedashakti@gmail.com',
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              width: 116,
+                              height: 47,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8),
+                                border: Border.all(
+                                  width: 1,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Box 1', // Text for the first box
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 8),
+                          Expanded(
+                            child: Container(
+                              width: 116,
+                              height: 47,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8),
+                                border: Border.all(
+                                  width: 1,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Box 2', // Text for the second box
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 8),
+                          Expanded(
+                            child: Container(
+                              width: 116,
+                              height: 47,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8),
+                                border: Border.all(
+                                  width: 1,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Box 3', // Text for the third box
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      ProfileMenuWidget(
+                        title: "Edit Profile Details",
+                        icon: Icons.edit,
+                        onPressed: () {},
+                      ),
+                      SizedBox(height: 10),
+                      ProfileMenuWidget(
+                        title: "Account",
+                        icon: Icons.account_box,
+                        onPressed: () {},
+                      ),
+                      SizedBox(height: 10),
+                      ProfileMenuWidget(
+                        title: "Wishlist",
+                        icon: Icons.favorite,
+                        onPressed: () {},
+                      ),
+                      SizedBox(height: 10),
+                      ProfileMenuWidget(
+                        title: "Settings",
+                        icon: Icons.settings,
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileMenuWidget extends StatelessWidget {
+  const ProfileMenuWidget({
+    required this.title,
+    required this.icon,
+    required this.onPressed,
+    this.endIcon = true,
+    this.textColor,
+    Key? key,
+  }) : super(key: key);
+
+  final String title;
+  final IconData icon;
+  final VoidCallback onPressed;
+  final bool endIcon;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        color: Colors.orange,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(color: textColor),
+      ),
+      trailing: Container(
+        width: 30,
+        height: 30,
+        child: Icon(
+          Icons.arrow_right,
+          size: 18.0,
+          color: Colors.grey,
+        ),
+      ),
+      onTap: onPressed,
     );
   }
 }
